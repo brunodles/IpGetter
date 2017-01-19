b4b210373a9cdc0df5bdb7cf577320c328520f72
Bruno de Lima <dlimaun@gmail.com>
2017-01-19 19:51:04 -0300

    Update IpGettertest to fix the test

--- src/test/kotlin/com/brunodles/ipgetter/IpGetterTest.kt
+++ src/test/kotlin/com/brunodles/ipgetter/IpGetterTest.kt
@@ -3,9 +3,9 @@ package com.brunodles.ipgetter
 import com.brunodles.oleaster.suiterunner.OleasterSuiteRunner
 import com.brunodles.test.helper.context
 import com.brunodles.test.helper.given
-import com.brunodles.test.helper.xgiven
 import com.mscharhag.oleaster.runner.StaticRunnerSupport.beforeEach
 import com.mscharhag.oleaster.runner.StaticRunnerSupport.it
+import groovy.lang.Closure
 import org.assertj.core.api.Assertions.assertThat
 import org.gradle.api.Project
 import org.gradle.api.Task
@@ -19,13 +19,15 @@ class IpGetterTest {
     init {
         given(Project::class) {
             beforeEach { project = ProjectBuilder.builder().build() }
+
             context("apply ${IpGetter::class.java.simpleName} plugin") {
-                beforeEach { project!!.pluginManager.apply("ipgetter") }
+                beforeEach { project!!.pluginManager.apply(IpGetter::class.java) }
+
                 it("should add an extension function to get the IP") {
-                    assertThat(project!!.extensions.getByName("localIpOr")).isInstanceOf(String::class.java)
+                    assertThat(project!!.extensions.getByName("localIpOr")).isInstanceOf(Closure::class.java)
                 }
                 it("should add a task to print properties") {
-                    assertThat(project!!.task("printIpProps")).isInstanceOf(Task::class.java)
+                    assertThat(project!!.tasks.getByName("printIpProps")).isInstanceOf(Task::class.java)
                 }
             }
         }
